55a0c514aa66ad6b047bca9121aea1680d6b2a4f
Røste <nora-lyngstad.roste@capgemini.com>
2020-10-16 14:08:22 +0200

    module completed

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,12 +8,17 @@ class Quiz extends Component{
         super(props);
         this.state = {quiz_position: 1}
     }
+    showNextQuestion(){
+        const prevState = this.state.quiz_position;
+        this.setState({quiz_position: prevState+1})
 
+    }
     render(){
         const isQuizEnd = this.state.quiz_position-1 === quizData.quiz_questions.length;
         return(
             <div>
-                {isQuizEnd?<QuizEnd/>:<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>}
+                {isQuizEnd?<QuizEnd/>:<QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)}
+                                                    quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>}
 
             </div>
         )
