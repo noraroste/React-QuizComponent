f3aecd165c55a6ef39bb49fb18dea213d21c5b1c
Røste <nora-lyngstad.roste@capgemini.com>
2020-10-16 12:27:31 +0200

    added quiz component, app renders

---
+++ src/Quiz.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+let quizData = require('./quiz_data.json')
+
+class Quiz extends Component{
+    constructor(){
+        super();
+        this.state = {quiz_position: 1}
+    }
+
+    render(){
+        let position = this.state.quiz_position;
+        console.log(position);
+        let resObj = quizData.quiz_questions.find(o=>o.id===position)
+        return(
+            <div>
+                <div className="QuizQuestion">
+                    {resObj.instruction_text}
+                </div>
+            </div>
+        )
+    }
+
+
+}export default Quiz
