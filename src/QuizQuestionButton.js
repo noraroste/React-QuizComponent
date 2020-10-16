1d17af501b35d950e8095f69995c3871acc5b81a
Røste <nora-lyngstad.roste@capgemini.com>
2020-10-16 13:35:32 +0200

    fix tekst i knapp

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -6,7 +6,7 @@ class QuizQuestionButton extends Component{
     render(){
         return(
         <li>
-            <button text = {this.props.button_text}/>
+            <button> {this.props.button_text}</button>
         </li>
         )
     }
