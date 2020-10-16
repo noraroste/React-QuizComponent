46308dd6b117f05b60d811dd38e7a0f838cf1d2a
Røste <nora-lyngstad.roste@capgemini.com>
2020-10-16 13:33:49 +0200

    added quizquestion button

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class QuizQuestionButton extends Component{
+
+
+    render(){
+        return(
+        <li>
+            <button text = {this.props.button_text}/>
+        </li>
+        )
+    }
+}
+export default QuizQuestionButton
